e42a502a17ecea76c25bfc473a04dc5ad1cbfddb
John Obertubbesing <oberthewhat@gmail.com>
2020-01-20 12:09:30 -0600

    working on news site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      stories: [],
+      input: "",
+      storyTitle: ""
+    };
+  }
+
+  onChange=(e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+
+  onSubmit= e =>  {
+    e.preventDefault()
+    this.setState ({
+      storyTitle: this.state.stories.map(articles => {
+      return articles.story_title
+      }),
+      input: ""
+    })
+
+  }
+
+  
+  
+  async componentDidMount() {
+    try{
+      const response = await fetch("http://hn.algolia.com/api/v1/search_by_date?query=...")
+      const stories = await response.json()
+    // .then(response => response.json())
+    this.setState({ stories: stories.hits })
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+    console.log(this.state.stories)
+    console.log(this.state.storyTitle) 
+    return (
+      <div>
+        <form className="example" onSubmit={this.onSubmit}>
+        <input type="text" name="search" onChange={this.onChange}/>
+        <button type="submit">Search</button>
+        </form>
+        {this.state.stories.map((articles) =>
+        <>
+         <h3>{articles.story_title}</h3>
+         <li key={articles.objectID}>{articles.author}</li> 
+        <a href={articles.story_url} key={articles.story_url}>{articles.story_url}</a>
+        </>
+        )}
+      </div>
+    );
+
+  }
 }
 
 export default App;
